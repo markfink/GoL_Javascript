9809ea590ef341a35f4e34612ae65087be823bea
Mark Fink <mark@mark-fink.de>
2012-04-01 11:28:30 +0200

    now the basic functionality of the board works

--- src/GoL.js
+++ src/GoL.js
@@ -28,9 +28,48 @@ gol = (function() {
 	}
 
 	Board.prototype.nextGeneration = function() {
-		return new Board(this.width, this.height);
+		var newBoard = new Board(this.width, this.height);
+		for (var x = 0; x < this.width; x++) {
+			for (var y = 0; y < this.height; y++) {
+			    var non = this.countNeighbors(x, y);
+			    if (non == 3 || (non == 2 && this.isAlive(x, y))) {
+			        newBoard.birth(x, y);
+			    }
+			}
+		}
+		return newBoard;
 	}
 	
+	Board.prototype.countNeighbors = function(x, y) {
+	    var count = 0;
+	    for (var i=-1; i<2; i++) {
+	        for (var j=-1; j<2; j++) {
+	            if (!(i == 0 && j == 0)) {
+    	            a = x + i;
+	                b = y + j;
+	                if (a == -1) {a = this.width -1;}
+	                if (b == -1) {b = this.height -1;}
+	                if (a == this.width) {a = 0;}
+	                if (b == this.height) {b = 0;}
+	                if (this.isAlive(a, b)) {count++;}
+	            }
+	        }
+	    }
+		return count;
+	}
+	
+	Board.prototype.batch = function(cells) {
+        //jstestdriver.console.log('Moin, moin');
+	    for (var i=0; i<cells.length; i++) {
+	        this.birth(cells[i][0], cells[i][1]);
+	    }
+    }
+    
+    Board.prototype.toString = function() {
+        return this.array.toString();
+    }
+
+
 	return {'Board': Board}
 
 }());
--- test/TestGoL.js
+++ test/TestGoL.js
@@ -8,18 +8,22 @@ TestCase('GameOfLifeTest', {
 
 	testDefaultCellStateOfANewBoardIsDead: function() {
 		var board = new gol.Board(5, 6);
-		assertTrue(board.isDead(1, 1));
+		for (var x = 0; x < 5; x++) {
+			for (var y = 0; y < 6; y++) {
+				assertTrue(board.isDead(x, y));
+			}
+		}
+	},
+	
+	testIsAlive: function() {
+		var board = new gol.Board(5, 6);
+		assertFalse(board.isAlive(1, 1));
 	},
 	
 	testCellStateOfAAliveCellIsAlive: function() {
 		var board = new gol.Board(5, 6);
 		board.birth(1, 2);
 		assertTrue(board.isAlive(1, 2));
-	},
-	
-	testDefaultCellStateOfANewBoardIsNotAlive: function() {
-		var board = new gol.Board(5, 6);
-		assertFalse(board.isAlive(1, 1));
 	},
 	
 	testNextGenerationOfEmptyBoardIsEmpty: function() {
@@ -30,6 +34,63 @@ TestCase('GameOfLifeTest', {
 				assertTrue(board.isDead(x, y));
 			}
 		}
-	}
+	},
+	
+	testOneAliveCellWithoutNeighborsDiesInNextGeneration: function() {
+		var board = new gol.Board(5, 6);
+		board.birth(1, 2);
+		var newBoard = board.nextGeneration();
+    	assertTrue(newBoard.isDead(1, 2));
+	},
+
+    testCountNeighbors: function() {
+        var board = new gol.Board(5, 6);
+        board.batch([[1, 1], [1, 2], [1, 3], [2, 1], [2, 3], [3, 1],
+            [3, 2], [3, 3]]);
+		assertEquals(8, board.countNeighbors(2, 2));
+    },
+
+    testCountNeighborsUpperLeftEdge: function() {
+        var board = new gol.Board(5, 6);
+        board.batch([[0, 1], [0, 5], [1, 0], [1, 1], [1, 5], [4, 0],
+            [4, 1], [4, 5]]);
+		assertEquals(8, board.countNeighbors(0, 0));
+    },
+
+    testCountNeighborsLowerRightEdge: function() {
+        var board = new gol.Board(5, 6);
+        board.batch([[0, 0], [0, 4], [0, 5], [3, 0], [3, 4], [3, 5],
+            [4, 0], [4, 4]]);
+		assertEquals(8, board.countNeighbors(4, 5));
+    },
+
+    testBatchWithOneElement: function() {
+        var board = new gol.Board(5, 6);
+		board.batch([[1, 2]]);
+    	assertTrue(board.isAlive(1, 2));
+    },
+
+    testBatchWithNoElement: function() {
+        var board = new gol.Board(5, 6);
+		board.batch([]);
+		for (var x = 0; x < 5; x++) {
+			for (var y = 0; y < 6; y++) {
+				assertTrue(board.isDead(x, y));
+			}
+		}
+    },
+
+    testBlinkerInNextGeneration: function() {
+		var board = new gol.Board(5, 6);
+		board.batch([[1, 2], [2, 2], [3, 2]]);
+		var newBoard = board.nextGeneration();
+        //jstestdriver.console.log('moin', newBoard.toString());
+    	assertTrue(newBoard.isDead(1, 2));
+    	assertTrue(newBoard.isDead(3, 2));
+    	assertTrue(newBoard.isAlive(2, 1));
+    	assertTrue(newBoard.isAlive(2, 2));
+    	assertTrue(newBoard.isAlive(2, 3));
+	},	
+
 
 });
